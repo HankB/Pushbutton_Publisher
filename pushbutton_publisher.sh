7613470f37a8f19f3cd07fdc3c1451d43d4f89fb
Hank Barta <hbarta@gmail.com>
2025-12-19 09:32:44 -0600

    GPIOD 2.2, use gpiomon, add timeout

--- pushbutton_publisher.sh
+++ pushbutton_publisher.sh
@@ -7,39 +7,40 @@ set -o nounset
 ############### end of Boilerplate
 
 state="unknown"
-gpio=20
-was_pressed="false"
+gpio=26
 broker="mqtt"
 hostname=$(hostname)
 topic="HA/$hostname/office/lighting_override"
+state="off"
 
-# Note: For this circuit the input reads '0' when the button is pressed
+# Note: For this circuit the input reads '2' when the button is pressed, 1 when released.
 
 while (:); do
-    button=$(gpioget -l /dev/gpiochip0 "$gpio")
+    button=$(/usr/bin/gpiomon -n 1 --idle-timeout 15m -F "%e" -p 10ms -c /dev/gpiochip0 "$gpio")
     # echo "$button"
-    if [ "$button" == "0" ] && [ "$was_pressed" == "false" ]; then
-        # echo "press detected"
-        was_pressed="true"
-        case "$state" in
-        "unknown")
-            state="on"
-            ;;
-        "off")
-            state="on"
-            ;;
-        "on")
-            state="off"
-            ;;
-        esac
-        # echo "state $state"
+    if [ -z "$button" ]; then
+        # echo timeout 
         timestamp=$(date +%s)
         message="{\"t\":$timestamp, \"override\":\"$state\", \"device\":\"pushbutton\"}"
         /usr/bin/mosquitto_pub -h "$broker" -t "$topic" -m "$message" || true
-
-    elif [ "$button" == "1" ]; then
-        was_pressed="false"
-        # echo "press off"
+    else
+        if [ "$button" == "2" ] ; then
+            # echo "press detected"
+            case "$state" in
+            "unknown")
+                state="on"
+                ;;
+            "off")
+                state="on"
+                ;;
+            "on")
+                state="off"
+                ;;
+            esac
+            # echo "state $state"
+            timestamp=$(date +%s)
+            message="{\"t\":$timestamp, \"override\":\"$state\", \"device\":\"pushbutton\"}"
+            /usr/bin/mosquitto_pub -h "$broker" -t "$topic" -m "$message" || true
+        fi
     fi
-    sleep 0.1 # bash can sleep for fracti0onal seconds! Who knew?
 done
